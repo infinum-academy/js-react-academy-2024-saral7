e8ad74126d7ee450674fa9022371af1d3db52997
Sara <sara.lazarusic@gmail.com>
2024-07-13 16:59:10 +0200

    assignment-2: modified ReviewForm to use useForm hook, added email info on a new review

--- tv-shows-app/src/components/feature/review/ReviewForm/ReviewForm.tsx
+++ tv-shows-app/src/components/feature/review/ReviewForm/ReviewForm.tsx
@@ -1,69 +1,56 @@
 import { IReview, IReviewItem } from "@/typings/review";
-import { Button, Flex, Input, NumberInput, Text, Textarea, useMediaQuery } from "@chakra-ui/react";
+import { Button, chakra, Flex, FormControl, Input, NumberInput, Text, Textarea, useMediaQuery } from "@chakra-ui/react";
 import ReviewStarsInput from "../ReviewStarsInput/ReviewStarsInput";
 import { useState } from "react";
+import { Form, useForm } from "react-hook-form";
+import useSWR from "swr";
+import { swrKeys } from "@/fetchers/swrKeys";
+import { authFetcher } from "@/fetchers/fetcher";
 
 export interface IOnPostFunction {
    addShowReview: (review : IReview) => void;
 }
 
+interface IReviewFormInputProps {
+   text: string
+}
+
 export default function ReviewForm({addShowReview} : IOnPostFunction) { 
-   // funkcija koja obraduje post iz forme
-   const addNewReview = () => {
-      let reviewInputText = document.getElementById('input-form-text') as HTMLTextAreaElement;
-      
-      if (!reviewInputText.value) return;
-
-      const newReview : IReview = {
-         text: reviewInputText.value,
-         rating: starsClicked
-      };
-      addShowReview(newReview); // 
-
-      // cleanup
-      reviewInputText.value = "";
-      setStarsClicked(1);
-   }
-
+   const {data} = useSWR<{user: {email: string}}>(swrKeys.me, authFetcher);
+   const {register, handleSubmit, reset} = useForm<IReviewFormInputProps>();
    const [starsClicked, setStarsClicked] = useState(1);
 
-   // funkcija koja definira kako izgledaju zvjezdice na klik
+   // pitanje: bi li i broj kliknutih zvjezdica trebao biti dio form inputa, ili je okej ostaviti ovako sa stateom pa rucno?
+   const addNewReview = ({text} : IReviewFormInputProps) => {
+      if (!data) return;
+      const newReview : IReview = {
+         text: text,
+         rating: starsClicked,
+         email: data.user.email
+      }
+      addShowReview(newReview);
+      setStarsClicked(1);
+      reset();
+   }
+
    const onStarClick = (index : number) => {
       setStarsClicked(index);
    }
 
-   return <Flex 
-            direction={'column'} 
-            width={'100%'} 
-            marginTop={2}
-            marginBottom={2}>
 
-            <Text 
-               fontWeight={'bold'} 
-               color={'white'} 
-               marginBottom={1}>
-                  Reviews
-            </Text>
+   return (
+      <chakra.form onSubmit={handleSubmit(addNewReview)}>
+         <FormControl display="flex" flexDirection="column" width="100%" marginTop={2} marginBottom={2}>
+            <Text fontWeight="bold" color="white" marginBottom={1}>Reviews</Text>
 
-            <Textarea 
-               id = 'input-form-text'
-               backgroundColor={'white'} 
-               placeholder="Add review" 
-               width='100%'
-               marginBottom={1}
-               paddingTop={1}
-               >
-            </Textarea>
-
-            <Flex alignItems={'center'} marginBottom={1} data-testid="stars-input"> {/* test nije pronalazio ovaj data-testid kada je on bio u ReviewStarsInput komponenti zapisan */}
-               <ReviewStarsInput label = 'Rating' value={starsClicked} onChange = {onStarClick} />
+            <Textarea {...register("text")} backgroundColor="white"placeholder="Add review" width="100%" marginBottom={1} paddingTop={1} />
+               
+            <Flex alignItems="center" marginBottom={1} data-testid="stars-input"> {/* test nije pronalazio ovaj data-testid kada je on bio u ReviewStarsInput komponenti zapisan */}
+               <ReviewStarsInput label = "Rating" value={starsClicked} onChange = {onStarClick} />
             </Flex>
 
-            <Button 
-               width={'30%'} 
-               borderRadius={'10px'}
-               onClick={addNewReview}>
-                  Post
-            </Button>
-         </Flex>
+            <Button type="submit" width="30%" borderRadius="10px">Post</Button>
+         </FormControl>
+      </chakra.form> 
+   )
 }
--- tv-shows-app/src/components/feature/review/ReviewItem/ReviewItem.tsx
+++ tv-shows-app/src/components/feature/review/ReviewItem/ReviewItem.tsx
@@ -1,6 +1,5 @@
-import { IReview, IReviewItem } from "@/typings/review";
-import { Button, Card, CardBody, Flex, Show, Text } from "@chakra-ui/react";
-import { Fragment } from "react";
+import { IReview } from "@/typings/review";
+import { Avatar, AvatarGroup, Button, Card, Flex, Text } from "@chakra-ui/react";
 import ReviewStarsInput from "../ReviewStarsInput/ReviewStarsInput";
 
 export interface IReviewItemProps {
@@ -11,6 +10,10 @@ export interface IReviewItemProps {
 export default function ReviewItem({review, onDelete} : IReviewItemProps) {
    return <Card padding={1} backgroundColor={'lightblue'} color={'white'}>
       <Flex direction={'column'} gap = {1}>
+         <Flex alignItems="center">
+            <Avatar height="32px" width="32px" name={review.email} marginRight={1}/>
+            <Text fontWeight="bold">{review.email}</Text>
+         </Flex>
          <Text data-testid="text">{review.text}</Text>
          <ReviewStarsInput label = {`${review.rating} / 5`} value={review.rating} onChange={() => {return;}}></ReviewStarsInput>
          {/*<Show above='768px'>     
